c7bd82941c30e4ad3f1481d4c4cb0142b771a428
arthuryan08 <arthuryan08@gmail.com>
2023-09-26 23:00:35 -0300

    style: lucide react lib and header done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import { Button } from "./components/ui/button";
+import { Github } from 'lucide-react'
+import { Separator } from "./components/ui/separator";
 
 export function App() {
   return (
-    <Button>Hello!!</Button>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+        
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvolvido com 💜 no NLW por Arthur Yan</span>
+
+          <Separator orientation="vertical" className="h-6"/>
+
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2"/>
+            Github</Button>
+        </div>
+      </div>
+    </div>
   )
 }
