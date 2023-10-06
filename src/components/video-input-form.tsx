4354c5d4dd1e1b7501481f768a15765e41284b89
arthuryan08 <arthuryan08@gmail.com>
2023-10-05 23:47:18 -0300

    feat: video preview and get prompts input done

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -3,25 +3,70 @@ import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { ChangeEvent, useState, useMemo, FormEvent, useRef } from "react"
 
 export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null)
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget
+
+    if (!files) {
+      return
+    }
+
+    const selectedFile = files[0]
+
+    setVideoFile(selectedFile)
+  }
+
+  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt = promptInputRef.current?.value
+
+    if (!videoFile) {
+      return
+    }
+
+    //converter o video em áudio
+
+  }
+
+  const previewURL = useMemo(() => {
+    if (!videoFile) {
+      return undefined
+    }
+
+    return URL.createObjectURL(videoFile)
+
+  }, [videoFile])
+
   return (
-    <form className="space-y-6">
+    <form onSubmit={handleUploadVideo} className="space-y-6">
       <label
         htmlFor="video"
-        className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-white/5"
+        className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-white/5"
       >
-        <FileVideo className="w-4 h-4" />
-        Selecione um vídeo
+        {videoFile ? (
+          <video src={previewURL} controls={false} className="pointer-events-none absolute inset-0" />
+        ) : (
+          <>
+            <FileVideo className="w-4 h-4" />
+            Selecione um vídeo
+          </>
+        )}
       </label>
 
-      <input type="file" id="video" accept="video/mp4" className="sr-only" />
+      <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected} />
 
       <Separator />
 
       <div className="space-y-2">
         <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
         <Textarea
+          ref={promptInputRef}
           id="transcription_prompt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras-chave mencionadas no video separadas por vírgula (,)"
